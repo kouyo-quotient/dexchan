5d14b64c02d587926c0d4b9eade0e03b190086ff
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-10-15 16:52:01 -0600

    Remove unused constant

--- src/main/java/com/github/kouyoquotient/utils/Constants.java
+++ src/main/java/com/github/kouyoquotient/utils/Constants.java
@@ -6,7 +6,6 @@ public final class Constants {
 
     public static final long CURRENT_SERVER_ID = 0L;
     public static final long SUPPORT_CHANNEL = 0L;
-    public static final long announceReceiveChannel = 0;
     public static final String JOIN_EVENT_WEBHOOK_URL = "";
     public static final String ANNOUNCEMENT_WEBHOOK_URL = "";
     public static final long SCANLATOR_ROLE = 0L;
