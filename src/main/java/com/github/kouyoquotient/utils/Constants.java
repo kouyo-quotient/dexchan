650da7663821ee94800b181b195dce39c1ea124f
kouyo-quotient <soydrexgamesfx69@gmail.com>
2022-10-16 12:21:42 -0600

    Template for Constants.java

---
+++ src/main/java/com/github/kouyoquotient/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.github.kouyoquotient.utils;
+
+import java.awt.*;
+
+public final class Constants {
+
+    public static final long CURRENT_SERVER_ID = 0L;
+    public static final long SUPPORT_CHANNEL = 0L;
+    public static final long announceReceiveChannel = 0L;
+    public static final String JOIN_EVENT_WEBHOOK_URL = "";
+    public static final String ANNOUNCEMENT_WEBHOOK_URL = "";
+    public static final long SCANLATOR_ROLE = 0L;
+    public static final long LECTOR_ROLE = 0L;
+    public static final long SCANLATOR_EMOJI_ID = 0L;
+    public static final long LECTOR_EMOJI_ID = 0L;
+    public static final long NEUTRAL_EMOJI_ID = 0L;
+    public static final Color DISCORD_BG_COLOR = new Color(56, 57, 61);
+    public static final Color USER_JOIN_COLOR = new Color(58, 145, 90);
+
+}
