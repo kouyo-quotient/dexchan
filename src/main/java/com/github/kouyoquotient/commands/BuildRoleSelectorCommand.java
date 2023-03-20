6ea02ff3e12855adfa1ec89adb29b32a6aa7fc25
kouyo-quotient <soydrexgamesfx69@gmail.com>
2023-03-19 19:26:35 -0700

    Change role selector message to non-embed and other general tweaks

--- src/main/java/com/github/kouyoquotient/commands/BuildRoleSelectorCommand.java
+++ src/main/java/com/github/kouyoquotient/commands/BuildRoleSelectorCommand.java
@@ -6,7 +6,8 @@ import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.component.ActionRow;
 import org.javacord.api.entity.message.component.SelectMenu;
 import org.javacord.api.entity.message.component.SelectMenuOption;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.message.mention.AllowedMentions;
+import org.javacord.api.entity.message.mention.AllowedMentionsBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -15,6 +16,24 @@ import java.util.ArrayList;
 import static com.github.kouyoquotient.Main.logger;
 
 public class BuildRoleSelectorCommand implements MessageCreateListener {
+    AllowedMentions allowedMentions = new AllowedMentionsBuilder()
+            .setMentionEveryoneAndHere(false)
+            .setMentionUsers(true)
+            .setMentionRoles(false)
+            .build();
+
+    String message = String.valueOf(new MessageBuilder()
+            .setAllowedMentions(allowedMentions)
+            .append("""
+                    __**Selector de roles**__
+                    Todos los roles aqu\u00ED son gratis y opcionales. Elige el que quieras:
+                                        
+                    :writing_hand: <@&968318880927850566>: Miembro de uno, dos o m\u00E1s grupos scanlation/fansub.
+                    :books: <@&968318841430085772>: Lector activo y aficionado.
+                    
+                    <:twitter:1068974812229288027> **Twitter Posts**: __Rol no disponible__
+                    <:mangadex:1003474388315803710> **MangaDex Posts**: __Rol no disponible__
+                    """).getStringBuilder());
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
@@ -27,30 +46,15 @@ public class BuildRoleSelectorCommand implements MessageCreateListener {
 
             logger.info("Received command for buildroleselector instruction");
 
-            EmbedBuilder embed = new EmbedBuilder()
-                    .setTitle("Elige tu destino")
-                    .setDescription(
-                            EmojiParser.parseToUnicode(
-                                    """
-                                            En nuestra versi\u00F3n del mundo existe una facci\u00F3n en una constante guerra de caos y destrucci\u00F3n contra un enemigo com\u00FAn. Los miembros de esta facci\u00F3n son...
-                                                            
-                                            :writing_hand: <@&968318880927850566>: Criaturas del abismo de las traducciones. Todos son venerados por sus subordinados y seguidores debido a que son capaces de entender lenguajes at\u00E1vicos.
-                                            :books: <@&968318841430085772>: Son conocidos en el abismo de las traducciones por fungir como catalizador y poseer vastos conocimientos de los manuscritos de [    ].
-                                            <:neutral:1003474388315803710> **Neutral:** Seres sin ning\u00FAn tipo de poder especial. Veneran a las criaturas del abismo de las traducciones.
-                                                            
-                                            \u00BFParticipar\u00E1s en esta guerra?
-                                            """))
-                    .setFooter("", "https://cdn.discordapp.com/attachments/864265368447746098/864274483664388136/84dc32a3-355b-4f6d-865c-c08d0c2ec6c4.png");
-//                    .setColor(DISCORD_BG_COLOR);
-
             ArrayList<SelectMenuOption> iSelectMenuOptions = new ArrayList<>();
             iSelectMenuOptions.add(SelectMenuOption.create("Scanlator", "scanlator", "Seleccionar rol Scanlator", EmojiParser.parseToUnicode(":writing_hand:")));
             iSelectMenuOptions.add(SelectMenuOption.create("Lector", "lector", "Seleccionar rol Lector", EmojiParser.parseToUnicode(":books:")));
             // You can't add custom emojis like above, so I'll leave this one like this for now.
-            iSelectMenuOptions.add(SelectMenuOption.create("Neutral", "neutral", "Esta opci\u00F3n remover\u00E1 tus roles seleccionados", api.getKnownCustomEmojiOrCreateCustomEmoji(1003474388315803710L, "neutral", false)));
+            iSelectMenuOptions.add(SelectMenuOption.create("Limpiar roles", "neutral", "Esta opci\u00F3n remover\u00E1 tus roles seleccionados", api.getKnownCustomEmojiOrCreateCustomEmoji(1068975870338924625L, "delete", false)));
 
             new MessageBuilder()
-                    .addEmbed(embed)
+                    .setAllowedMentions(allowedMentions)
+                    .append(message)
                     .addComponents(ActionRow.of(
                             SelectMenu.createStringMenu(
                                     "iroleselector",
