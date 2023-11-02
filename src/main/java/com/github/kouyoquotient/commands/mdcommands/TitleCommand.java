84e8bde917eaf69e4a71c62383b86862421f6d80
kouyo <koubellii@proton.me>
2023-11-02 11:42:23 -0700

    Remove redundant code & deprecations from TitleCommand

--- src/main/java/com/github/kouyoquotient/commands/mdcommands/TitleCommand.java
+++ src/main/java/com/github/kouyoquotient/commands/mdcommands/TitleCommand.java
@@ -1,6 +1,5 @@
 package com.github.kouyoquotient.commands.mdcommands;
 
-import com.google.gson.*;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import kong.unirest.HttpResponse;
@@ -14,10 +13,9 @@ import org.javacord.api.listener.interaction.SlashCommandCreateListener;
 import org.javacord.api.listener.message.MessageCreateListener;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class TitleCommand implements MessageCreateListener, SlashCommandCreateListener {
     private static final Logger logger = LogManager.getRootLogger();
@@ -43,12 +41,7 @@ public class TitleCommand implements MessageCreateListener, SlashCommandCreateLi
                             "&includes[]=artist")
                     .asString();
 
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            JsonElement jsonElement = JsonParser.parseString(httpResponse.getBody());
-
             try {
-                Path pathToJsonFile = Path.of("response.json");
-                Files.write(pathToJsonFile, gson.toJson(jsonElement).getBytes());
                 Object parsedJson = Configuration.defaultConfiguration().jsonProvider().parse(httpResponse.getBody());
 
                 String mainTitle = JsonPath.read(parsedJson, "$.data.attributes.title.en");
@@ -57,26 +50,13 @@ public class TitleCommand implements MessageCreateListener, SlashCommandCreateLi
                 Object mangaAuthorObject = JsonPath.read(parsedJson, "$.data.relationships[?(@.type == 'author')].attributes.name");
                 Object mangaArtistObject = JsonPath.read(parsedJson, "$.data.relationships[?(@.type == 'artist')].attributes.name");
 
-                String coverArt = coverArtObject.toString().replaceAll("[\\[\\]\"]", "");
+                String coverArtUUID = coverArtObject.toString().replaceAll("[\\[\\]\"]", "");
                 String authorNames = mangaAuthorObject.toString().replaceAll("[\\[\\]\"]", "");
                 String artistNames = mangaArtistObject.toString().replaceAll("[\\[\\]\"]", "");
 
-                URL imageURL = new URL("https://uploads.mangadex.org/covers/"+UUIDValue+"/"+coverArt+".256.jpg");
-                InputStream inputStream = imageURL.openStream();
-                Path imagePath = Path.of("cover.jpg");
-                Files.copy(inputStream, imagePath);
-                inputStream.close();
-
-                if (Files.exists(imagePath)) {
-                    try {
-                        logger.info("Leftover files found, deleting...");
-                        Files.delete(imagePath);
-                        logger.info("Files deleted.");
-                    } catch (IOException e) {
-                        logger.error(e);
-                    }
-                }
-
+                String coverArtURL = "https://uploads.mangadex.org/covers/"+UUIDValue+"/"+coverArtUUID+".256.jpg";
+                URI uri = new URI(coverArtURL);
+                URL coverArtURItoURL = uri.toURL();
 
                 EmbedBuilder responseEmbed = new EmbedBuilder()
                         .setTitle(mainTitle)
@@ -84,15 +64,15 @@ public class TitleCommand implements MessageCreateListener, SlashCommandCreateLi
                         .setAuthor("MangaDex", "https://mangadex.org/manga/"+UUIDValue,"https://cdn.discordapp.com/attachments/1000809614377500832/1169690488564088972/mangadex-logo.png")
                         .addField("Autores:", authorNames)
                         .addInlineField("Artistas:", artistNames)
-                        .setThumbnail(new URL("https://uploads.mangadex.org/covers/"+UUIDValue+"/"+coverArt+".256.jpg").openStream());
-
+                        .setThumbnail(coverArtURItoURL.openStream());
 
                 event.getInteraction()
-                        .respondLater().thenAccept(interactionOriginalResponseUpdater -> {
-                            interactionOriginalResponseUpdater.addEmbed(responseEmbed).update();
-                        });
+                        .respondLater()
+                        .thenAccept(interactionOriginalResponseUpdater ->
+                                interactionOriginalResponseUpdater.addEmbed(responseEmbed)
+                                        .update());
 
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 throw new RuntimeException(e);
             }
         }
