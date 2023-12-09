ae001f2ffe4b9c21dc83e3f73be0d529ac7b2726
kouyo-quotient <koubellii@proton.me>
2023-12-09 12:56:36 -0700

    Slight improvement to Ping command

--- src/main/java/com/github/kouyoquotient/commands/PingCommand.java
+++ src/main/java/com/github/kouyoquotient/commands/PingCommand.java
@@ -1,5 +1,7 @@
 package com.github.kouyoquotient.commands;
 
+import kong.unirest.Unirest;
+import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.listener.interaction.SlashCommandCreateListener;
 
@@ -8,7 +10,18 @@ public class PingCommand implements SlashCommandCreateListener {
     @Override
     public void onSlashCommandCreate(SlashCommandCreateEvent event) {
         if(event.getSlashCommandInteraction().getFullCommandName().equalsIgnoreCase("ping")){
-            event.getInteraction().createImmediateResponder().setContent("pong!").respond();
+            Unirest.config().instrumentWith(requestSummary -> {
+                long startMilis = System.currentTimeMillis();
+                return (responseSummary,exception) ->
+                        new MessageBuilder()
+                                .append("Response summary:")
+                                .append("\n- (MangaDexAPI) Status: "+responseSummary.getStatus())
+                                .append("\n- (MangaDexAPI) Approx. response time: "+(System.currentTimeMillis()-startMilis)+"ms")
+                                .send(event.getSlashCommandInteraction().getChannel().orElseThrow());
+            });
+
+            Unirest.config().defaultBaseUrl("https://api.mangadex.org");
+            Unirest.get("/ping").asEmpty();
         }
     }
 }
